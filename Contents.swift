bc93403bcdb1464253d0bb9a406ff30579e508be
EnverKangal <kangal1enver@gmail.com>
2025-05-11 04:49:56 +0300

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+var ilce:String? = "Saruhanlı"
+var kıta:String? = "Asya"
+var faks = 1234
+var postaKodu:Int? = 45800
+var instAdresi = "Pashenver"
+var calıstıgıBolum:String? = "Ogrencı"
+var urunMıktarı = 12
+var musterıSoyadı:String? = "Kangal"
+var odemeMıktarı = 43.200
+var dogumTarıhı:String? = "28.03.2003"
+var borculuMU:Bool? = true
+var evlıMI:Bool? = false
+var yorumVıdeo:Bool? = true
+var odemeSaatı:Double? = 13.15
+var eftMıktarı:Double? = 12.30
+var satılanMıktar:Int? = 12
+var telefonModelı:String? = "iPhone 12"
+var dergıAdı:String? = "Hürriyet"
+var basımTarıhı:String? = "28.03.2021"
+var zamMıktarı:Double? = 12.30
+var daıreSsayıs:Int? = 13
+var enlem:Int? = 38
+var boylam:Int? = 38
+var yemekAdı:String? = "Kabuska"
+var urunFıyatı:Double? = 99.12
+var basınc:Int? = 12
+var etkınlıkGunu:String? = "28.03.2021"
+var odemeGunu:String? = "28.03.2021"
+var yolculukTarıhı:String? = "28.03.2021"
+var mahalleAdı = "Cengız topuz"
+var otobusHattı = "7A"
+var kullanılanDakıka:Int? = 300
+var kargoTakıpNo:String? = "123456789"
+var kuponSuresı:Int? = 5
+var kuponKodu:Int? = 123
+var faturaTarıhı:String? = "22.02.2002"
+var sırket:Bool? = false
+var vıdeoAdı:String? = "Apple"
+var muzıkSuresı:Double? = 12.45
+var mekanPuanı:Int? = 4
+var dosyaAdı:String? = "MAcos"
+var resımFormatı:String? = "jpeg"
+var renk:String? = "Kırmızı"
+var renkKodu:String? = "#FF0000"
+var bılgısayarModelı:String? = "MacBook Pro"
+var ekranBoyutu:Double? = 12.56
+var kullanımSuresı:Int = 8
+print("Ilce:\(ilce)")
+print("Kıta:\(kıta)")
+print("Faks:\(faks)")
+print("Posta Kodu : \(postaKodu)")
+print("Instgram : \(instAdresi)")
+print("Calıstıgı Bolum : \(calıstıgıBolum)")
+print("Ürün Mıktarı :\(urunMıktarı)")
+print("Müsterı Soyadı : \(musterıSoyadı)")
+print ("Dergı Adı : \(dergıAdı)")
+print("Yemek Adı : \(yemekAdı)")
